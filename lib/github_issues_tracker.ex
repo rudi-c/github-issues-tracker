10681248a1388d2ca36127f978133b160035a10a
Rudi Chen <rudichen@gmail.com>
2017-05-24 11:31:58 -0400

    Initial commit

---
+++ lib/github_issues_tracker.ex
@@ -0,0 +1,18 @@
+defmodule GithubIssuesTracker do
+  @moduledoc """
+  Documentation for GithubIssuesTracker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GithubIssuesTracker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/github_issues_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule GithubIssuesTrackerTest do
+  use ExUnit.Case
+  doctest GithubIssuesTracker
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
